1ca8e53720bbd2a60676f75a68f9fc503bc7dd7d
Vinícius Nunes de Oliveira <br.viniciusnunes@gmail.com>
2025-06-28 04:43:31 -0300

    feat: add CORS support

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import process from 'node:process';
+import cors from 'cors';
 import timetableRouter from './routes/timetable.route.js';
 
 const app = express();
 
+app.use(cors());
 app.use('/api/', timetableRouter);
 
 const PORT = process.env.PORT || 3000;
